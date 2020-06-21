df484e3f8bfff937139326b3ff9bb0be8045a33d
jessykalage <jessyka.l@thoughtworks.com>
2020-06-21 11:04:29 -0300

    aula tdd

--- src/prime_calculator.py
+++ src/prime_calculator.py
@@ -1,4 +1,13 @@
 
 class PrimeCalculator():
     def calculate(self, number):
-        return False
+        if number == 1:
+            return False
+        else:
+            for index in range(2, number - 1):
+                if self.is_divisible(number, index):
+                    return False
+        return True
+
+    def is_divisible(self, number, divisor):
+        return number % divisor == 0
--- test/test_primeCalculator.py
+++ test/test_primeCalculator.py
@@ -3,3 +3,11 @@ from src.prime_calculator import PrimeCalculator
 def test_deve_retornar_False_quando_1():
     primeCalculator = PrimeCalculator()
     assert False == primeCalculator.calculate(1)
+
+def test_deve_retornar_True_quando_2():
+    primeCalculator = PrimeCalculator()
+    assert True == primeCalculator.calculate(2)
+
+def test_deve_retornar_False_quando_4():
+    primeCalculator = PrimeCalculator()
+    assert False == primeCalculator.calculate(4)
